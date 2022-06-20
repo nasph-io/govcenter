35cd466186c485e68435dd96cff2064177c51938
Edgar Silva <edgar@skalena.com>
2022-06-20 18:30:14 -0300

    Update database.js
    Updating for SQL Lite

--- metad/config/database.js
+++ metad/config/database.js
@@ -1,13 +1,11 @@
+const path = require('path');
+
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: 'sqlite',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'metad'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'mysql'),
-      ssl: env.bool('DATABASE_SSL', false),
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
+    useNullAsDefault: true,
   },
 });
